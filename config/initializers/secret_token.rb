e5ea0d6104a89635dc3eeb5754d969ca035c8c83
edma8378 <edward.maton@gmail.com>
2013-11-20 14:14:21 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hw5::Application.config.secret_key_base = '6174b08e3c5f697147edcd67c4bde3e27dee5304d175bd8e05f8a071a609deea2d160fe8eeac050a432958052fa6781fdd9e074b5957f1f09bbf0f783a059e6c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hw5::Application.config.session_store :cookie_store, key: '_hw5_session'
